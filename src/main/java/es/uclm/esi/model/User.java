59f31a6fac0f19065347d363b0e961aa3abc2ee8
CarrivasUclm <carlos.rivas1@alu.uclm.es>
2020-12-12 15:40:18 +0100

    Ahora sí

--- src/main/java/es/uclm/esi/model/User.java
+++ src/main/java/es/uclm/esi/model/User.java
@@ -13,72 +13,123 @@ import org.springframework.data.mongodb.core.mapping.Document;
 
 @Document(collection = "users")
 public class User {
-  @Id
-  private String id;
+	@Id
+	private String id;
 
-  @NotBlank
-  @Size(max = 20)
-  private String username;
-  
+	@NotBlank
+	@Size(max = 20)
+	private String username;
 
-  @NotBlank
-  @Size(max = 50)
-  @Email
-  private String email;
+	@NotBlank
+	@Size(max = 50)
+	@Email
+	private String email;
 
-  @NotBlank
-  @Size(max = 120)
-  private String password;
+	@NotBlank
+	@Size(max = 120)
+	private String password;
 
-  @DBRef
-  private Set<Role> roles = new HashSet<>();
+	@DBRef
+	private Set<Role> roles = new HashSet<>();
 
-  public User() {
-  }
+	private String nombre;
 
-  public User(String username, String email, String password) {
-    this.username = username;
-    this.email = email;
-    this.password = password;
-  }
+	private String apellidos;
 
-  public String getId() {
-    return id;
-  }
+	private int telefono;
 
-  public void setId(String id) {
-    this.id = id;
-  }
+	private String dni;
 
-  public String getUsername() {
-    return username;
-  }
+	public User() {
+	}
 
-  public void setUsername(String username) {
-    this.username = username;
-  }
+	public User(String username, String email, String password) {
+		this.username = username;
+		this.email = email;
+		this.password = password;
+	}
 
-  public String getEmail() {
-    return email;
-  }
+	public User(String username, String email, String password, String nombre, String apellidos, int telefono,
+	        String dni) {
+		this.username = username;
+		this.email = email;
+		this.password = password;
+		this.nombre = nombre;
+		this.apellidos =apellidos;
+		this.telefono = telefono;
+		this.dni = dni;
+	}
 
-  public void setEmail(String email) {
-    this.email = email;
-  }
+	public String getId() {
+		return id;
+	}
 
-  public String getPassword() {
-    return password;
-  }
+	public void setId(String id) {
+		this.id = id;
+	}
 
-  public void setPassword(String password) {
-    this.password = password;
-  }
+	public String getUsername() {
+		return username;
+	}
 
-  public Set<Role> getRoles() {
-    return roles;
-  }
+	public void setUsername(String username) {
+		this.username = username;
+	}
 
-  public void setRoles(Set<Role> roles) {
-    this.roles = roles;
-  }
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public Set<Role> getRoles() {
+		return roles;
+	}
+
+	public void setRoles(Set<Role> roles) {
+		this.roles = roles;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getApellidos() {
+		return apellidos;
+	}
+
+	public void setApellidos(String apellidos) {
+		this.apellidos = apellidos;
+	}
+
+	public int getTelefono() {
+		return telefono;
+	}
+
+	public void setTelefono(int telefono) {
+		this.telefono = telefono;
+	}
+
+	public String getDni() {
+		return dni;
+	}
+
+	public void setDni(String dni) {
+		this.dni = dni;
+	}
+	
 }
