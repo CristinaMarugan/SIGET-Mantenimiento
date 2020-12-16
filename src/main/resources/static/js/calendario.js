dcf0e388843deb3cc3f05dc14b1f0ee538015be9
CarrivasUclm <carlos.rivas1@alu.uclm.es>
2020-12-16 17:43:17 +0100

    Mejora calidad

--- src/main/resources/static/js/calendario.js
+++ src/main/resources/static/js/calendario.js
@@ -1,7 +1,7 @@
 //Arrays de datos:
-meses = ["enero","febrero","marzo","abril","mayo","junio","julio","agosto","septiembre","octubre","noviembre","diciembre"];
-lasemana = ["Domingo","Lunes","Martes","Miércoles","Jueves","Viernes","Sábado"]
-diassemana = ["lun","mar","mié","jue","vie","sáb","dom"];
+var meses = ["enero","febrero","marzo","abril","mayo","junio","julio","agosto","septiembre","octubre","noviembre","diciembre"];
+var lasemana = ["Domingo","Lunes","Martes","Miércoles","Jueves","Viernes","Sábado"]
+var diassemana = ["lun","mar","mié","jue","vie","sáb","dom"];
 
 //Tras cargarse la página ...
 window.onload = function() {
@@ -12,26 +12,26 @@ window.onload = function() {
     setRol();
 
 	//Fecha actual
-	hoy = new Date(); //objeto fecha actual
-	diasemhoy = hoy.getDay(); //dia semana actual
-	diahoy = hoy.getDate(); //dia mes actual
-	meshoy = hoy.getMonth(); //mes actual
-	annohoy = hoy.getFullYear(); //año actual
+	var hoy = new Date(); //objeto fecha actual
+	/*var diasemhoy = hoy.getDay(); //dia semana actual
+	var diahoy = hoy.getDate(); //dia mes actual
+	var meshoy = hoy.getMonth(); //mes actual*/
+	var annohoy = hoy.getFullYear(); //año actual
 
 	// Elementos del DOM: en cabecera de calendario 
-	tit = document.getElementById("titulos"); //cabecera del calendario
-	ant = document.getElementById("anterior"); //mes anterior
-	pos = document.getElementById("posterior"); //mes posterior
+	/*var tit = document.getElementById("titulos"); //cabecera del calendario
+	var ant = document.getElementById("anterior"); //mes anterior
+	var pos = document.getElementById("posterior"); //mes posterior*/
 
 	// Elementos del DOM en primera fila
-	f0 = document.getElementById("fila0");
+	//var f0 = document.getElementById("fila0");
 
 	//Formulario: datos iniciales:
 	document.buscar.buscaanno.value = annohoy;
 
 	// Definir elementos iniciales:
-	mescal = meshoy; //mes principal
-	annocal = annohoy //año principal
+	/*var mescal = meshoy; //mes principal
+	var annocal = annohoy //año principal*/
 
 	//Iniciar calendario:
 	cabecera(); 
@@ -43,8 +43,8 @@ window.onload = function() {
 //Cabecera del calendario
 function cabecera() {
     tit.innerHTML = meses[mescal]+" de "+annocal;
-    mesant = mescal-1; //mes anterior
-    mespos = mescal+1; //mes posterior
+    var mesant = mescal-1; //mes anterior
+    var mespos = mescal+1; //mes posterior
 
     if(mesant < 0) {mesant = 11;}
     if(mespos > 11) {mespos = 0;}
@@ -55,8 +55,8 @@ function cabecera() {
 
 //Primera línea de tabla: días de la semana.
 function primeralinea() {
-    for(i = 0; i < 7; i++) {
-        celda0 = f0.getElementsByTagName("th")[i];
+    for(var i = 0; i < 7; i++) {
+        var celda0 = f0.getElementsByTagName("th")[i];
         celda0.innerHTML = diassemana[i]
    }
 }
@@ -68,26 +68,26 @@ function escribirdias() {
 	var jsonreuniones = getReunionesMesC();
 
     //Buscar dia de la semana del dia 1 del mes:
-    primeromes = new Date(annocal,mescal,"1") //buscar primer día del mes
-    prsem = primeromes.getDay() //buscar día de la semana del día 1
+    var primeromes = new Date(annocal,mescal,"1") //buscar primer día del mes
+    var prsem = primeromes.getDay() //buscar día de la semana del día 1
     prsem--; //adaptar al calendario español (empezar por lunes)
 
     if(prsem == -1) {prsem = 6;}
 	//Buscar fecha para primera celda:
-    diaprmes = primeromes.getDate() 
-    prcelda = diaprmes-prsem; //restar días que sobran de la semana
-    empezar = primeromes.setDate(prcelda) //empezar= tiempo UNIX 1ª celda
-    diames = new Date() //convertir en fecha
+    var diaprmes = primeromes.getDate() 
+    var prcelda = diaprmes-prsem; //restar días que sobran de la semana
+    var empezar = primeromes.setDate(prcelda) //empezar= tiempo UNIX 1ª celda
+    var diames = new Date() //convertir en fecha
     diames.setTime(empezar); //diames=fecha primera celda.
 
     //Recorrer las celdas para escribir el día:
-    for(i = 1; i < 7; i++) { //localizar fila
-        fila=document.getElementById("fila"+i);
-        for(j = 0; j < 7; j++) {
-            midia = diames.getDate() 
-            mimes = diames.getMonth()
-            mianno = diames.getFullYear()
-	        celda = fila.getElementsByTagName("td")[j];
+    for(var i = 1; i < 7; i++) { //localizar fila
+        var fila=document.getElementById("fila"+i);
+        for(var j = 0; j < 7; j++) {
+            var midia = diames.getDate() 
+            var mimes = diames.getMonth()
+            var mianno = diames.getFullYear()
+	        var celda = fila.getElementsByTagName("td")[j];
             celda.innerHTML = midia;
     	    celda.setAttribute('id',midia);
 
@@ -108,7 +108,7 @@ function escribirdias() {
             }
 
             //Destacar la fecha actual
-            if(mimes == meshoy && midia == diahoy && mianno == annohoy ) { ;
+            if(mimes == meshoy && midia == diahoy && mianno == annohoy ) {
               	celda.innerHTML = "<cite title='Fecha Actual'>"+ midia +"</cite>";
                 //celda.style.border = "thick solid #9370DB";
                 celda.style.font = "bold 18pt arial";
@@ -122,7 +122,7 @@ function escribirdias() {
             diames.setDate(midia);
 
             if(jsonreuniones != 0){
-	            for(ii = 0; ii < jsonreuniones.reuniones.length; ii++){ //Resalta cuando hay una reunión ese día
+	            for(var ii = 0; ii < jsonreuniones.reuniones.length; ii++){ //Resalta cuando hay una reunión ese día
 	              	if(celda.getAttribute("id") == jsonreuniones.reuniones[ii]){
 	              		celda.style.backgroundColor = "#CACCD3";
 	               	}
@@ -137,31 +137,31 @@ function escribirdias() {
 
 //Ver mes anterior
 function mesantes() {
-    nuevomes = new Date() //nuevo objeto de fecha
+    var nuevomes = new Date() //nuevo objeto de fecha
     primeromes--; //Restamos un día al 1 del mes visualizado
     nuevomes.setTime(primeromes) //cambiamos fecha al mes anterior 
-    mescal = nuevomes.getMonth() //cambiamos las variables que usarán las funciones
-    annocal = nuevomes.getFullYear()
+    /*var mescal = nuevomes.getMonth() //cambiamos las variables que usarán las funciones
+    var annocal = nuevomes.getFullYear()*/
     cabecera() //llamada a funcion de cambio de cabecera
     escribirdias() //llamada a funcion de cambio de tabla.
 }
 
 //ver mes posterior
 function mesdespues() {
-    nuevomes = new Date() //nuevo obejto fecha
-    tiempounix = primeromes.getTime() //tiempo de primero mes visible
+    var nuevomes = new Date() //nuevo obejto fecha
+    var tiempounix = primeromes.getTime() //tiempo de primero mes visible
     tiempounix = tiempounix+(45*24*60*60*1000) //le añadimos 45 días 
     nuevomes.setTime(tiempounix) //fecha con mes posterior.
-    mescal = nuevomes.getMonth() //cambiamos variables
-    annocal = nuevomes.getFullYear()
+    /*var mescal = nuevomes.getMonth() //cambiamos variables
+    var annocal = nuevomes.getFullYear()*/
     cabecera() //escribir la cabecera 
     escribirdias() //escribir la tabla
 }
 
 //volver al mes actual
 function actualizar() {
-    mescal = hoy.getMonth(); //cambiar a mes actual
-    annocal = hoy.getFullYear(); //cambiar a año actual 
+    /*var mescal = hoy.getMonth(); //cambiar a mes actual
+    var annocal = hoy.getFullYear(); //cambiar a año actual */
     cabecera() //escribir la cabecera
     escribirdias() //escribir la tabla
 }
@@ -169,22 +169,22 @@ function actualizar() {
 //ir al mes buscado
 function mifecha() {
     //Recoger dato del año en el formulario
-    mianno = document.buscar.buscaanno.value; 
+    var mianno = document.buscar.buscaanno.value; 
     //recoger dato del mes en el formulario
-    listameses = document.buscar.buscames;
-    opciones = listameses.options;
-    num = listameses.selectedIndex
-    mimes = opciones[num].value;
+    var listameses = document.buscar.buscames;
+    var opciones = listameses.options;
+    var num = listameses.selectedIndex
+    var mimes = opciones[num].value;
     //Comprobar si el año está bien escrito
     if(isNaN(mianno) || mianno < 1) { 
         //año mal escrito: mensaje de error
         alert("El año no es válido:\n debe ser un número mayor que 0")
     } else { //año bien escrito: ver mes en calendario:
-        mife = new Date(); //nueva fecha
+        var mife = new Date(); //nueva fecha
         mife.setMonth(mimes); //añadir mes y año a nueva fecha
         mife.setFullYear(mianno);
-        mescal = mife.getMonth(); //cambiar a mes y año indicados
-        annocal = mife.getFullYear();
+        /*var mescal = mife.getMonth(); //cambiar a mes y año indicados
+        var annocal = mife.getFullYear();*/
         cabecera() //escribir cabecera
         escribirdias() //escribir tabla
     }
--- src/main/resources/static/js/index.js
+++ src/main/resources/static/js/index.js
@@ -1,65 +1,67 @@
 $(document).ready(function() {
-    $("#mostrarLogin").click(function() {
-        $("#divLogin").show();
-        $("#divRegistro").hide()
-    }),
-    $("#mostrarRegistro").click(function() {
-        $("#divLogin").hide();
-        $("#divRegistro").show()
-    })
+	$("#mostrarLogin").click(function() {
+		$("#divLogin").show();
+		$("#divRegistro").hide()
+	});
+});
+$(document).ready(function() {
+	$("#mostrarRegistro").click(function() {
+		$("#divLogin").hide();
+		$("#divRegistro").show()
+	});
 });
 
 function SignUp() {
-    var info = {
-        type : "Register",
-        username : $("#SignUpName").val(),
-        email : $("#SignUpEmail").val(),
-        password : $("#pwd1").val(),
-        
-    };
-    var data = {
-            data : JSON.stringify(info),
-            url : "api/auth/signup",
-            type : "post",
-            contentType: 'application/json',
-            success : function() {
-                alert("Registro realizado");
-            },
-            error : function(response) {
-                alert("Error en el registro del usuario");
-            }
-        };
-        $.ajax(data);
+	var info = {
+		type: "Register",
+		username: $("#SignUpName").val(),
+		email: $("#SignUpEmail").val(),
+		password: $("#pwd1").val(),
+
+	};
+	var data = {
+		data: JSON.stringify(info),
+		url: "api/auth/signup",
+		type: "post",
+		contentType: 'application/json',
+		success: function() {
+			alert("Registro realizado");
+		},
+		error: function(response) {
+			alert("Error en el registro del usuario");
+		}
+	};
+	$.ajax(data);
 }
 
 function LogIn() {
-    var info = {
-        type : "Login",
-        username : $("#loginUsername").val(),
-        password : $("#loginUserPassword").val()
-    };
-    
-    var data = {
-        data : JSON.stringify(info),
-        url : "api/auth/signin",
-        type : "post",
-        contentType: 'application/json',
-        success : function(response) {
-        localStorage.setItem('jwt','Bearer '+response.accessToken);//guardo el token que luego habra que enviar en el header de las peticiones
+	var info = {
+		type: "Login",
+		username: $("#loginUsername").val(),
+		password: $("#loginUserPassword").val()
+	};
 
-        localStorage.setItem('accessToken','acesoAlToken');
-        window.location.href="calendario.html";
-        },
-        error : function(response) {
-            alert("Error en la autenticacion del usuario");
-        }
-    };
-    $.ajax(data);
-    console.log(data);
+	var data = {
+		data: JSON.stringify(info),
+		url: "api/auth/signin",
+		type: "post",
+		contentType: 'application/json',
+		success: function(response) {
+			localStorage.setItem('jwt', 'Bearer ' + response.accessToken);//guardo el token que luego habra que enviar en el header de las peticiones
+
+			localStorage.setItem('accessToken', 'acesoAlToken');
+			window.location.href = "calendario.html";
+		},
+		error: function(response) {
+			alert("Error en la autenticacion del usuario");
+		}
+	};
+	$.ajax(data);
+	console.log(data);
 }
 
 
 
 function requestToken() {
-    alert("En requestToken");
+	alert("En requestToken");
 }
